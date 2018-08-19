a87f414d579a99ac990ed4fa949dca35f4d4d653
liu466531065 <42435901+liu466531065@users.noreply.github.com>
2018-08-19 13:33:13 +0800

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "bit-love")
+set(CN_CURRENCY_DISPLAY_NAME "Bit-Love")
+set(CN_CURRENCY_TICKER "Bit-Love")
